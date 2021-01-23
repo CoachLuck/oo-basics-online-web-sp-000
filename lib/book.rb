743f2987e5a25a186abb80ec3a848ce97d091759
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-23 22:55:19 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,10 @@
 class Book
-  attr_accessor :title, :name, :genre, :pagecount
+  attr_accessor :title, :name, :genre, :page_count
 
   def initialize(title)
     @title = title
+  end
+
+  def turn_page
   end
 end
